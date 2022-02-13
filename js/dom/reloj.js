2e487c22f3822380751406b5ba00e734f4bfccca
Sebastian <jsrubianoj@gmail.com>
2022-02-13 14:08:07 -0500

    reloj terminado

--- js/dom/reloj.js
+++ js/dom/reloj.js
@@ -1,3 +1,42 @@
 /* **********     Curso JavaScript: 82. DOM: Ejercicios Prácticos | Reloj Digital - #jonmircha     ********** */
-export function digital
-export function
+const d=document;
+export function digitalClock(clock,btnPlay,btnStop){
+    let clockTempo;
+    d.addEventListener('click',(e)=>{
+        if(e.target.matches(btnPlay)){
+            clockTempo=setInterval(()=>{
+                let clockHour=new Date().toLocaleTimeString();
+                d.querySelector(clock).innerHTML=`<h3>${clockHour}</h3>`;
+            },1000);
+            e.target.disabled=true;
+        }
+        if(e.target.matches(btnStop)){
+            clearInterval(clockTempo);
+            d.querySelector(clock).innerHTML='<h3></h3>';
+            d.querySelector(btnPlay).disabled=false;
+        }
+    }
+    )}
+
+
+
+export function alarm(sound, btnPlay,btnStop){
+    let alarmTempo;
+    const $alarm=d.createElement('audio');
+    $alarm.src=sound;
+    d.addEventListener('click',(e)=>{
+        if(e.target.matches(btnPlay)){
+            alarmTempo=setTimeout(()=>{
+            $alarm.play();
+        },2000);
+            e.target.disabled=true;
+        }
+        if(e.target.matches(btnStop)){
+            clearTimeout(alarmTempo);
+            $alarm.pause();
+            $alarm.currentTime=0;
+            d.querySelector(btnPlay).disabled=false;
+        }
+    }
+    )}
+
--- js/index_dom.js
+++ js/index_dom.js
@@ -1,9 +1,12 @@
 /* **********     Curso JavaScript: 80. DOM: Ejercicios Prácticos | Presentación - #jonmircha     ********** */
 // Hamburguesa
 import hamburgerMenu from "./dom/menu_hamburguesa.js";
+import { digitalClock,alarm } from "./dom/reloj.js";
 const d=document;
 
 d.addEventListener('DOMContentLoaded',(e)=>{
     hamburgerMenu(".panel-btn",".panel",".menu a");
+    digitalClock("#reloj","#activar-reloj","#desactivar-reloj");
+    alarm('assets/alarma.mp3','#activar-alarma','#desactivar-alarma');
 }
 )
