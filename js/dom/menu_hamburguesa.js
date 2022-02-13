949c052d7cf83ecac8f142033ca78d2f6c09954f
Sebastian <jsrubianoj@gmail.com>
2022-02-12 22:28:53 -0500

    menu-hamburguesa 1

--- js/dom/menu_hamburguesa.js
+++ js/dom/menu_hamburguesa.js
@@ -1,1 +1,5 @@
 /* **********     Curso JavaScript: 81. DOM: Ejercicios Prácticos | Menú de Hamburguesa - #jonmircha     ********** */
+export default function hamburgerMenu(panelBtn,panel){
+    const d=document;
+    d.addEventListener('click')
+}
--- js/index_dom.js
+++ js/index_dom.js
@@ -1,1 +1,4 @@
+import hamburgerMenu from "./dom/menu_hamburguesa.js";
+
 /* **********     Curso JavaScript: 80. DOM: Ejercicios Prácticos | Presentación - #jonmircha     ********** */
+hamburgerMenu(".panel-btn",".panel");
