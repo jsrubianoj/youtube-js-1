259459c3b964db983eea62614e50f0d0ba7bcb67
Sebastian <jsrubianoj@gmail.com>
2022-02-14 21:00:33 -0500

    initial commit

--- js/dom/teclado.js
+++ js/dom/teclado.js
@@ -1,1 +1,6 @@
 /* **********     Curso JavaScript: 85. DOM: Ejercicios | Eventos del Teclado (movimientos y colisiones) - #jonmircha     ********** */
+const d = document;
+
+export function shortcuts(e){
+    console.log(e);
+}
